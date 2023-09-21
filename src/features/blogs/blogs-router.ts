a9c78f0d0814277070948659432e2a60f5ce355e
Ivan Malinovsky <imalinovsky@tradingview.com>
2023-09-21 17:20:09 +0300

    small refactor of blogs router

--- src/features/blogs/blogs-router.ts
+++ src/features/blogs/blogs-router.ts
@@ -6,16 +6,16 @@ import { inputValidation } from '../../middlewares/input-validation';
 import { blogsRepository } from './blogs-repository';
 import { Controller } from '../../common/controller';
 
-export const blogsController = new Controller(blogsRepository);
+const nameValidation = body('name').notEmpty().isString().isLength({ max: 15 });
+const descriptionValidation = body('description').notEmpty().isString().isLength({ max: 500 });
+const urlValidation = body('websiteUrl').notEmpty().isURL().isLength({ max: 100 });
+
+const blogsController = new Controller(blogsRepository);
 const blogsRouter = Router()
 
 blogsRouter.use(checkAuthorization);
 
 blogsRouter.get('/', blogsController.getAll);
-
-const nameValidation = body('name').notEmpty().isString().isLength({ max: 15 });
-const descriptionValidation = body('description').notEmpty().isString().isLength({ max: 500 });
-const urlValidation = body('websiteUrl').notEmpty().isURL().isLength({ max: 100 });
 
 blogsRouter.post('/',
 	nameValidation,
