df04a3431d93da05f926e4adc590bf57645a76d5
Ivan Malinovsky <imalinovsky@tradingview.com>
2023-10-02 19:00:36 +0300

    add user exists validation

--- src/features/users/users-validation.ts
+++ src/features/users/users-validation.ts
@@ -1,5 +1,28 @@
 import { body } from 'express-validator';
+import { UsersQueryRepository } from './users-query-repository';
 
-export const loginValidation = body('login').isString().notEmpty().isLength({ min: 3, max: 10}).matches(/^[a-zA-Z0-9_-]*$/)
-export const passwordValidation = body('password').isString().notEmpty().isLength({min: 6, max: 20})
-export const emailValidation = body('email').isString().notEmpty().isEmail();
+const usersQueryRepository = new UsersQueryRepository();
+
+export const loginValidation = body('login').isString().notEmpty().isLength({
+	min: 3,
+	max: 10
+}).matches(/^[a-zA-Z0-9_-]*$/).custom(async (value) => {
+	const user = await usersQueryRepository.getByLogin(value);
+
+	if (user) {
+		return Promise.reject('User with such login already exists');
+	}
+
+	return Promise.resolve(true);
+});
+
+export const passwordValidation = body('password').isString().notEmpty().isLength({ min: 6, max: 20 })
+export const emailValidation = body('email').isString().notEmpty().isEmail().custom(async (value) => {
+	const user = await usersQueryRepository.getByEmail(value);
+
+	if (user) {
+		return Promise.reject('User with such email already exists');
+	}
+
+	return Promise.resolve(true);
+});
